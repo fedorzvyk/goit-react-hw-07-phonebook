a8bed69f9314838ec7526db1cea0ad02b57a2ede
fedorzvyk <fedorzvyk@gmail.com>
2023-01-28 18:10:41 +0200

    added styles

--- src/components/ContactListItem/ContactListItem.styled.jsx
+++ src/components/ContactListItem/ContactListItem.styled.jsx
@@ -6,6 +6,10 @@ export const Item = styled.li`
   justify-content: space-between;
   gap: ${p => p.theme.space[4]}px;
   font-weight: ${p => p.theme.fontWeights.bold};
+  @media screen and (max-width: 480px) {
+    flex-direction: column;
+    gap: ${p => p.theme.space[2]}px;
+  }
 `;
 export const ItemName = styled.span`
   /* max-width: 200px; */
